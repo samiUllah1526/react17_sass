13c559cf79221b14024dbfa83ecc3dc53441717b
fawad-invoo <sami.ullah@invozone.com>
2023-07-16 14:53:37 +0500

    make state/variable names consistent

--- src/game/index.jsx
+++ src/game/index.jsx
@@ -3,40 +3,40 @@ import { getRandomArray } from '../utils'
 import styles from './index.module.scss'
 
 export const Game = () => {
-  const [cards, setCards] = useState(getRandomArray())
-  const [selectedCardsIDs, setSelectedCardsIDs] = useState([])
-  const [matchedCardsIDs, setMatchedCardsIDs] = useState([])
+  const [tiles, setTiles] = useState(getRandomArray())
+  const [selectedTilesIDs, setSelectedTilesIDs] = useState([])
+  const [matchedTilesIDs, setMatchedTilesIDs] = useState([])
 
   const handleReset = () => {
-    setCards(getRandomArray())
-    setSelectedCardsIDs([])
-    setMatchedCardsIDs([])
+    setTiles(getRandomArray())
+    setSelectedTilesIDs([])
+    setMatchedTilesIDs([])
   }
 
   const handleSelectedCard = (id) => {
-    const tempSelectedIDs = selectedCardsIDs.includes(id) ? selectedCardsIDs : [...selectedCardsIDs, id]
-    const tempSelectedCards = cards.filter(item => tempSelectedIDs.includes(item.id))
+    const tempSelectedIDs = selectedTilesIDs.includes(id) ? selectedTilesIDs : [...selectedTilesIDs, id]
+    const tempSelectedTiles = tiles.filter(item => tempSelectedIDs.includes(item.id))
 
-    if (tempSelectedCards.length !== 2) {
-      setSelectedCardsIDs([tempSelectedIDs[0]])
+    if (tempSelectedTiles.length !== 2) {
+      setSelectedTilesIDs([tempSelectedIDs[0]])
       return
     }
 
-    if (tempSelectedCards[0].value !== tempSelectedCards[1].value) {
-      setSelectedCardsIDs([])
+    if (tempSelectedTiles[0].value !== tempSelectedTiles[1].value) {
+      setSelectedTilesIDs([])
       return
     }
 
-    setMatchedCardsIDs(prev => [...prev, ...tempSelectedIDs])
-    setSelectedCardsIDs([])
+    setMatchedTilesIDs(prev => [...prev, ...tempSelectedIDs])
+    setSelectedTilesIDs([])
   }
 
 
   return (
     <div className={styles['wrapper']}>
-      <div className={styles["card"]}>
-        {cards.map((item) => (
-          <Tile key={item.id} {...{ item, selectedCardsIDs, matchedCardsIDs }} onClick={handleSelectedCard} />
+      <div className={styles["board"]}>
+        {tiles.map((item) => (
+          <Tile key={item.id} {...{ item, selectedTilesIDs, matchedTilesIDs }} onClick={handleSelectedCard} />
         ))}
       </div>
       <button className={styles['btn']} onClick={handleReset}>Reset</button>
@@ -45,17 +45,16 @@ export const Game = () => {
 }
 
 
-const Tile = ({ item, selectedCardsIDs, matchedCardsIDs, onClick }) => {
-
-  const classes = styles["tile"] + " " + (matchedCardsIDs.includes(item.id) || selectedCardsIDs.includes(item.id) ? styles["selected_tile"] : '')
-
+const Tile = ({ item, selectedTilesIDs, matchedTilesIDs, onClick }) => {
+  const selected = matchedTilesIDs.includes(item.id) || selectedTilesIDs.includes(item.id) ? styles["selected_tile"] : ''
+  
   const handleClick = (e) => {
     e.preventDefault()
     onClick(item.id)
   }
 
   return (
-    <div className={classes} onClick={handleClick}>
+    <div className={`${styles["tile"]} ${selected}`} onClick={handleClick}>
       {item.value}
     </div>
   )
